2622d5e6ba093b3396d06551f23db83bb9ab3796
Unknown <skrzynska.konstancja@gmail.com>
2020-04-29 16:50:40 +0200

    slider

---
+++ javascript/slider.js
@@ -0,0 +1,7 @@
+let slideTime = $('.slider').length * 1000;
+$('.slider').slice(1).hide();
+setInterval(function() {
+    "use strict";
+    $('.slider').first().fadeOut(1000).next().fadeIn(1000).end().appendTo('section')
+    // $('.slider').first().fadeOut(1000).next().fadeIn(1000)
+}, slideTime);
